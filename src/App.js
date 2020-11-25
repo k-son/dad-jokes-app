a3c249c4384e72fec1e8e7f1cdf24d1cbd35bed3
Jacek Jarczok <kson.eu@gmail.com>
2020-11-25 12:46:28 +0100

    Create JokeList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React, {Component} from 'react';
+import JokeList from './components/JokeList/JokeList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <JokeList />
     </div>
   );
 }
