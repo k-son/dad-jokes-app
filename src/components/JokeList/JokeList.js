acc9723ad885ef29266b6b6e103a60b2465b33fe
Jacek Jarczok <kson.eu@gmail.com>
2020-11-26 14:58:51 +0100

    Create spinner

--- src/components/JokeList/JokeList.js
+++ src/components/JokeList/JokeList.js
@@ -12,7 +12,8 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
+      loading: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -37,6 +38,7 @@ class JokeList extends Component {
       });
     }
     this.setState(st => ({
+      loading: false,
       jokes: [...st.jokes, ...jokes]
     }),
       // optional second argument, a callback function executed once setState is completed
@@ -56,10 +58,19 @@ class JokeList extends Component {
   }
 
   handleClick() {
-    this.getJokes();
+    this.setState({loading: true }, this.getJokes);
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div className="JokeList-spinner">
+          <i className="far fa-8x fa-laugh fa-spin" />
+          <h1 className="JokeList-title">Loading...</h1>
+        </div>
+      );
+    }
+
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
