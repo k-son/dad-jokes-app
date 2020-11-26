043246fdc781ed6b5c8db1a113cd7353bf387cc6
Jacek Jarczok <kson.eu@gmail.com>
2020-11-26 14:23:25 +0100

    Develop button onClick method

--- src/components/JokeList/JokeList.js
+++ src/components/JokeList/JokeList.js
@@ -14,12 +14,13 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     if (this.state.jokes.length === 0) this.getJokes();
   }
-  
+
   async getJokes() {
     let jokes = [];
     // while beacouse if there are duplicates (you can get more instances of thesame joke) it will loop further
@@ -35,8 +36,12 @@ class JokeList extends Component {
         votes: 0
       });
     }
-    this.setState({ jokes: jokes });
-    window.localStorage.setItem('jokes', JSON.stringify(jokes));
+    this.setState(st => ({
+      jokes: [...st.jokes, ...jokes]
+    }),
+      // optional second argument, a callback function executed once setState is completed
+      () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
   }
 
   handleVote(id, delta) {
@@ -44,7 +49,14 @@ class JokeList extends Component {
       jokes: st.jokes.map(j => 
         j.id === id ? {...j, votes: j.votes + delta} : j
       )
-    }));
+    }),
+      // optional second argument, a callback function executed once setState is completed
+      () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
+  }
+
+  handleClick() {
+    this.getJokes();
   }
 
   render() {
@@ -55,7 +67,7 @@ class JokeList extends Component {
             <span>Dad</span> Jokes
           </h1>
           <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
